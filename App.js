d1821940fd2a5ae0b499af708f0f330e43262524
JuliaNolli <julia.noli1b3@gmail.com>
2024-02-26 12:06:27 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Julia Eduarda Noli</Text>
+      <Text>Ana Clara de Andrade</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
